1908fe4801f2a830dc75af47e1950ae74b498197
bhuwan1718 <bhuwankhatri459@gmail.com>
2026-09-08 13:23:47 +0545

    admin profile enhanced

--- tourism_project/urls.py
+++ tourism_project/urls.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+
+admin.site.site_header = "Nepal Tourism E-Governance Portal"
+admin.site.site_title = "Nepal Tourism Admin"
+admin.site.index_title = "Department of Tourism — Administration Panel"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
